42bd45bce7a11bdd2afcb3b81116ed78cd80dab9
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-22 09:00:16 +0300

    Working with Arrays

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+  cats.push(name);
+};
+
+const destructivelyPrependCat = (name) => {
+  cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat=() => {
+    cats.pop();
+}
+const  destructivelyRemoveFirstCat =() => {
+    cats.shift();
+}
+const appendCat = (name) => {
+    return [...cats, name];
+}
+const prependCat = (name) => {
+    return [name, ...cats];
+}
+const removeLastCat = () => {
+    return cats.slice(0, -1);
+}
+const removeFirstCat = () => {
+    return cats.slice(1);
+}
